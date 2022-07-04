f878bce8e7444b8b44a8c357e831a59e9c21e3c6
Namtrung123 <trungcntt96@gmail.com>
2022-07-04 11:14:35 +0700

    boxmap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxmapComponent } from './boxmap/boxmap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxmapComponent
   ],
   imports: [
     BrowserModule,
